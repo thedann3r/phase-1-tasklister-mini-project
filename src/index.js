90be3558f3f6f9f8cce968b6d1ec9334b66ee017
dann <austindan168@gmail.com>
2024-10-06 22:10:55 +0300

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let myForm = document.getElementById("create-task-form")
+  myForm.addEventListener("submit", (eg) => {
+    eg.preventDefault()
+    let value = document.getElementById("new-task-description").value
+    let task = document.getElementById("tasks")
+    let taskItems = document.createElement("li")
+    taskItems.textContent = value
+    task.appendChild(taskItems)
+    let buton = document.createElement("button")
+    buton.textContent = "X"
+    buton.style.marginLeft  = "20px"
+    buton.style.backgroundColor = "black"
+    buton.style.color = "white"
+    buton.addEventListener("click", () =>{
+      buton.parentNode.remove()
+    })
+    taskItems.appendChild(buton)
+    myForm.reset()
+  })
 });
